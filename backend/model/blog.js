e598e5530468990d779e54435c37a0615e4764b9
vwayek <iamvivekfreelancer@gmail.com>
2026-06-18 12:09:06 +0530

    post route added

--- backend/model/blog.js
+++ backend/model/blog.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema({
     title: {
         type: String,
-        require: true
+        required: true
     },
     description: {
         type: String,
-        require: true
+        required: true
     }
 },{timestamps: true})
 
--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,36 @@ import {config} from 'dotenv'
 import dbConnect from "./db/db.js";
 import Blog from "./model/blog.js";
 
+// server setup
 const app = express()
 const port = 4000;
 
+// db and env config
 config()
 dbConnect()
 
+// middlewares
+app.use(express.urlencoded({extended: false}))
+
+// routes
 app.get("/", (req,res)=>{
     res.send("hello from server")
 })
 
-app.post("/create", (req,res)=>{
-    res.send("created")
+app.post("/create", async (req,res)=>{
+    try{
+        const newTitle = req.body.title
+        const newDescription = req.body.description
+
+        const {title, description, createdAt, updatedAt} = await Blog.create({
+            title: newTitle,
+            description: newDescription
+        })
+        res.json({success: true, data: {title, description, createdAt, updatedAt}}).status(201)
+    }catch(error){
+        res.json({success: false, error: "not created"}).status(500)
+    }
+    
 })
 
 app.patch("/edit/:id", (req,res)=>{
