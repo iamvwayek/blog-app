94ffbbb6488abd68b55bf4989f0ee515a5786874
vwayek <iamvivekfreelancer@gmail.com>
2026-06-22 11:23:43 +0530

    routes added

--- backend/server.js
+++ backend/server.js
@@ -20,28 +20,73 @@ app.get("/", (req,res)=>{
     res.send("hello from server")
 })
 
+app.get("/blogs", async(req,res)=>{
+    try {
+        const result = await Blog.find({},{__v: 0})
+        res.status(200).json({success: true, data: result})   
+    } catch (error) {
+        res.status(500).json({success: false, msg: "Not able to fetch!"})
+    }
+
+})
+
 app.post("/create", async (req,res)=>{
     try{
         const newTitle = req.body.title
         const newDescription = req.body.description
 
-        const {title, description, createdAt, updatedAt} = await Blog.create({
+        if(!newTitle || !newDescription){
+            return res.status(400).json({success: false, msg: "Something is missing!"})
+        }
+
+        const { _id, title, description, createdAt, updatedAt} = await Blog.create({
             title: newTitle,
             description: newDescription
         })
-        res.json({success: true, data: {title, description, createdAt, updatedAt}}).status(201)
+
+        res.status(201).json({success: true, data: {_id, title, description, createdAt, updatedAt}})
     }catch(error){
-        res.json({success: false, error: "not created"}).status(500)
+        res.status(500).json({success: false, msg: "Not created!"})
     }
     
 })
 
-app.patch("/edit/:id", (req,res)=>{
-    res.send("edited")
+app.put("/edit/:id", async(req,res)=>{
+
+    try {
+        const id = req.params.id
+        const updatedTitle = req.body.title
+        const updatedDescription = req.body.description
+    
+        if(!updatedTitle || !updatedDescription){
+                return res.status(400).json({success: false, msg: "Something is missing!"})
+            }
+            
+            const {_id, title, description, createdAt, updatedAt} = await Blog.findByIdAndUpdate(
+                {_id: id},
+                {title: updatedTitle, description: updatedDescription},
+                {new: true}
+            )
+            res.status(200).json({success: true, data: {_id, title, description, createdAt, updatedAt}})
+            
+    } catch (error) {
+         res.status(500).json({success: false, msg: "Unable to update!"})
+        }
+
+    
 })
 
-app.delete("/delete/:id", (req,res)=>{
-    res.send("deleted")
+app.delete("/delete/:id", async(req,res)=>{
+    try {
+        const _id = req.params.id;
+        const result = await Blog.findByIdAndDelete(_id)
+        if(!result){
+            return res.status(400).json({success: false, msg: "Blog not found!"})
+        }
+        res.status(200).json({success: true, msg: "Deleted successfully!"})
+    } catch (error) {
+         res.status(500).json({success: false, msg: "Unable to delete!"})   
+    }
 })
 
 app.listen(port, (err)=>{
